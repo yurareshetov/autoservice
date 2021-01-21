51ce5667adb578acea9c634c277d8dec9a7f34fc
yurareshetov <darkaskyl@mail.ru>
2021-01-21 10:04:41 +0300

    autoservice

--- autoreshetov/MainWindow.xaml.cs
+++ autoreshetov/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace autoreshetov
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
@@ -35,6 +37,57 @@ namespace autoreshetov
         {
             get { return _ServiceList; }
             set { _ServiceList = value; }
+        }
+        private Boolean _IsAdminMode = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // публичный геттер, который меняет текущий режим (Админ/не Админ)
+        public Boolean IsAdminMode
+        {
+            get { return _IsAdminMode; }
+            set
+            {
+                _IsAdminMode = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("AdminModeCaption"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("AdminVisibility"));
+                }
+            }
+        }
+        // этот геттер возвращает текст для кнопки в зависимости от текущего режима
+        public string AdminModeCaption
+        {
+            get
+            {
+                if (IsAdminMode) return "Выйти из режима\nАдминистратора";
+                return "Войти в режим\nАдминистратора";
+            }
+        }
+        private void AdminButton_Click(object sender, RoutedEventArgs e)
+        {
+            // если мы уже в режиме Администратора, то выходим из него 
+            if (IsAdminMode) IsAdminMode = false;
+            else
+            {
+                // создаем окно для ввода пароля
+                var InputBox = new InputBoxWindow("Введите пароль Администратора");
+                // и показываем его как диалог (модально)
+                if ((bool)InputBox.ShowDialog())
+                {
+                    // если нажали кнопку "Ok", то включаем режим, если пароль введен верно
+                    IsAdminMode = InputBox.InputText == "0000";
+                }
+            }
+        }
+        public string AdminVisibility
+        {
+            get
+            {
+                if (IsAdminMode) return "Visible";
+                return "Collapsed";
+            }
         }
     }
 }

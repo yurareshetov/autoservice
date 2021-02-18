4f00016039a4b54b765d5945a848ea418583385e
MaksimMih <vip.peta.1998@mail.ru>
2021-02-18 10:26:14 +0300

    autoservice

--- autoreshetov/MainWindow.xaml.cs
+++ autoreshetov/MainWindow.xaml.cs
@@ -37,24 +37,32 @@ namespace autoreshetov
         {
             get
             {
+
                 var FilteredServiceList = _ServiceList.FindAll(item =>
-    item.DiscountFloat >= CurrentDiscountFilter.Item1 &&
-    item.DiscountFloat < CurrentDiscountFilter.Item2);
+                item.DiscountFloat >= CurrentDiscountFilter.Item1 &&
+                item.DiscountFloat < CurrentDiscountFilter.Item2);
+
                 if (SearchFilter != "")
                     FilteredServiceList = FilteredServiceList.Where(item =>
                         item.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1 ||
                         item.DescriptionString.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1).ToList();
 
+
                 if (SortPriceAscending)
                     return FilteredServiceList.OrderBy(item => Double.Parse(item.CostWithDiscount)).ToList();
+
                 else
                     return FilteredServiceList.OrderByDescending(item => Double.Parse(item.CostWithDiscount)).ToList();
             }
-            set { _ServiceList = value; }
+            set
+            {
+                _ServiceList = value;
+            }
         }
         private Boolean _IsAdminMode = false;
 
         public event PropertyChangedEventHandler PropertyChanged;
+
 
         // публичный геттер, который меняет текущий режим (Админ/не Админ)
         public Boolean IsAdminMode
@@ -68,6 +76,7 @@ namespace autoreshetov
                     PropertyChanged(this, new PropertyChangedEventArgs("AdminModeCaption"));
                     PropertyChanged(this, new PropertyChangedEventArgs("AdminVisibility"));
                 }
+
             }
         }
         // этот геттер возвращает текст для кнопки в зависимости от текущего режима
@@ -106,29 +115,26 @@ namespace autoreshetov
         private Boolean _SortPriceAscending = true;
         public Boolean SortPriceAscending
         {
-            get { return _SortPriceAscending; }
+            get
+            {
+                return _SortPriceAscending;
+            }
             set
             {
                 _SortPriceAscending = value;
                 if (PropertyChanged != null)
                 {
                     PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
+
                 }
             }
         }
-            private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             SortPriceAscending = (sender as RadioButton).Tag.ToString() == "1";
         }
-        private List<Tuple<string, float, float>> FilterByDiscountValuesList =
-              new List<Tuple<string, float, float>>() {
-        Tuple.Create("Все записи", 0f, 1f),
-        Tuple.Create("от 0% до 5%", 0f, 0.05f),
-        Tuple.Create("от 5% до 15%", 0.05f, 0.15f),
-        Tuple.Create("от 15% до 30%", 0.15f, 0.3f),
-        Tuple.Create("от 30% до 70%", 0.3f, 0.7f),
-        Tuple.Create("от 70% до 100%", 0.7f, 1f)
-    };
         public List<string> FilterByDiscountNamesList
         {
             get
@@ -137,10 +143,21 @@ namespace autoreshetov
                     .Select(item => item.Item1)
                     .ToList();
             }
-        }
-        private Tuple<float, float> _CurrentDiscountFilter = Tuple.Create(float.MinValue, float.MaxValue);
 
-        public Tuple<float, float> CurrentDiscountFilter
+        }
+        private List<Tuple<string, double, double>> FilterByDiscountValuesList =
+            new List<Tuple<string, double, double>>()
+            {
+                Tuple.Create("Все записи", 0d, 1d),
+                Tuple.Create("от 0% до 5%", 0d, 0.05d),
+                Tuple.Create("от 5% до 15%", 0.05d, 0.15d),
+                Tuple.Create("от 15% до 30%", 0.15d, 0.3d),
+                Tuple.Create("от 30% до 70%", 0.3d, 0.7d),
+                Tuple.Create("от 70% до 100%", 0.7d, 1d)
+            };
+        private Tuple<double, double> _CurrentDiscountFilter = Tuple.Create(double.MinValue, double.MaxValue);
+
+        public Tuple<double, double> CurrentDiscountFilter
         {
             get
             {
@@ -153,16 +170,12 @@ namespace autoreshetov
                 {
                     // при изменении фильтра список перерисовывается
                     PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
                 }
             }
         }
-        private void DiscountFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            CurrentDiscountFilter = Tuple.Create(
-                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item2,
-                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item3
-            );
-        }
+
         private string _SearchFilter = "";
         public string SearchFilter
         {
@@ -174,6 +187,8 @@ namespace autoreshetov
                 {
                     // при изменении фильтра список перерисовывается
                     PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
                 }
             }
         }
@@ -183,6 +198,52 @@ namespace autoreshetov
             SearchFilter = SearchFilterTextBox.Text;
         }
 
+        private void DiscountFilterComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            if (DiscountFilterComboBox.SelectedIndex >= 0)
+                CurrentDiscountFilter = Tuple.Create(
+                    FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item2,
+                    FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item3
+
+                );
+        }
+        public int ServicesCount
+        {
+            get
+            {
+                return _ServiceList.Count;
+            }
+
+        }
+        public int FilteredServicesCount
+        {
+            get
+            {
+                return ServiceList.Count;
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var item = MainDataGrid.SelectedItem as Service;
+
+            // по условиям задачи нельзя удалять только те услуги, которые уже оказаны
+            // свойство ClientService ссылается на таблицу оказанных услуг
+            if (item.ClientService.Count > 0)
+            {
+                MessageBox.Show("Нельзя удалять услугу, она уже оказана");
+                return;
+            }
+
+            // метод Remove нужно завернуть в конструкцию try..catch, на случай, если 
+            // база спроектирована криво и нет каскадного удаления - это сделайте сами
+            Core.DB.Service.Remove(item);
+
+            // сохраняем изменения
+            Core.DB.SaveChanges();
+
+            // перечитываем изменившийся список, не забывая в сеттере вызвать PropertyChanged
+            ServiceList = Core.DB.Service.ToList();
+        }
     }
 }
-
